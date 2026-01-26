66cb8ce882e9baac2c8f5cde99736d3273aff1c4
dlewis123 <dslewis1317@gmail.com>
2026-01-26 01:41:24 -0500

    Initial raylib snake game project setup

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include "include/raylib.h"
+#include "include/raymath.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World" << endl;
+    return 0;
+}
