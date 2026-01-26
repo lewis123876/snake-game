54477ab3b32553e2691cac3796b588cd727eb5b6
dlewis123 <dslewis1317@gmail.com>
2026-01-26 02:27:47 -0500

    feat: add food class with randomly positioned food and image texture

--- main.cpp
+++ main.cpp
@@ -10,17 +10,54 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30;
 int cellCount = 25;
 
+class Food
+{
+public:
+    // rmber: numbers represent cells not pixels
+    Vector2 position;
+    Texture2D texture;
+    Food()
+    {
+        Image image = LoadImage("graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = GenerateRandomPos();
+    }
+
+    ~Food()
+    {
+        UnloadTexture(texture);
+    }
+
+    void Draw()
+    {
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+    }
+
+    Vector2 GenerateRandomPos()
+    {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{x, y};
+    }
+
+private:
+};
+
 int main()
 {
     cout << "Starting the game..." << endl;
     InitWindow(cellCount * cellSize, cellCount * cellSize, "Snake Game");
     SetTargetFPS(60);
 
+    Food food = Food();
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
         ClearBackground(green);
+        food.Draw();
 
         EndDrawing();
     }
