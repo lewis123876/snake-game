c92f149769114c8e6e4cf624d1b58e4fdccdb010
dlewis123 <dslewis1317@gmail.com>
2026-01-26 02:07:00 -0500

    refactor: change static values to a grid of cellCount and cellSize

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@ using namespace std;
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
 
+int cellSize = 30;
+int cellCount = 25;
+
 int main()
 {
     cout << "Starting the game..." << endl;
-    InitWindow(750, 750, "Snake Game");
+    InitWindow(cellCount * cellSize, cellCount * cellSize, "Snake Game");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false)
